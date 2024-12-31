ea5306de1a6f2b53397e255b561009388b683cd2
prasad-potdar <prasad.potdar@politicaldata.com>
2024-12-31 12:19:10 +0530

    initial commit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,10 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.config({
+    extends: ['next', 'prettier'],
+    ignorePatterns: ["deployment/k8s/**/*.yaml"],
+  }),
 ];
 
 export default eslintConfig;
